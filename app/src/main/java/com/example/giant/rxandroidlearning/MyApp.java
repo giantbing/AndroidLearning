f4c41759c847050000c94b5aef1ff6da08f924e6
giant <39505143@qq.com>
2017-03-21 18:00:19 +0800

    添加内存泄漏工具 LeakCanary

--- app/src/main/java/com/example/giant/rxandroidlearning/MyApp.java
+++ app/src/main/java/com/example/giant/rxandroidlearning/MyApp.java
@@ -3,13 +3,14 @@ package com.example.giant.rxandroidlearning;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
 
 /**@author giant
  * Created by giant on 2017/3/21.
  */
 
 public class MyApp extends Application {
-    public void onCreate() {
+   @Override public void onCreate() {
         super.onCreate();
         Stetho.initialize(
                 Stetho.newInitializerBuilder(this)
@@ -18,6 +19,13 @@ public class MyApp extends Application {
                         .enableWebKitInspector(
                                 Stetho.defaultInspectorModulesProvider(this))
                         .build());
+
+       if (LeakCanary.isInAnalyzerProcess(this)) {
+           // This process is dedicated to LeakCanary for heap analysis.
+           // You should not init your app in this process.
+           return;
+       }
+       LeakCanary.install(this);
     }
 
 }
