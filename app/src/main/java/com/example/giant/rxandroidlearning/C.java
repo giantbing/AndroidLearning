6f243a7e017264e7d3839f971702d16ce918c6a0
giant <39505143@qq.com>
2017-03-30 10:01:06 +0800

    增加C类用于保存全局常量类 将Application中需要在DEBUG模式执行的代码进行规范

---
+++ app/src/main/java/com/example/giant/rxandroidlearning/C.java
@@ -0,0 +1,11 @@
+package com.example.giant.rxandroidlearning;
+
+/**全局常量类
+ * Created by giant on 2017/3/30.
+ */
+
+public class C {
+
+    public static final Boolean DEBUG = BuildConfig.DEBUG;
+
+}
--- app/src/main/java/com/example/giant/rxandroidlearning/MyApp.java
+++ app/src/main/java/com/example/giant/rxandroidlearning/MyApp.java
@@ -30,21 +30,24 @@ public class MyApp extends Application {
                     .build();
 
         }
+        if (C.DEBUG){
 
-        Stetho.initialize(
-                Stetho.newInitializerBuilder(this)
-                        .enableDumpapp(
-                                Stetho.defaultDumperPluginsProvider(this))
-                        .enableWebKitInspector(
-                                Stetho.defaultInspectorModulesProvider(this))
-                        .build());
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(
+                                    Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(
+                                    Stetho.defaultInspectorModulesProvider(this))
+                            .build());
 
-       if (LeakCanary.isInAnalyzerProcess(this)) {
-           // This process is dedicated to LeakCanary for heap analysis.
-           // You should not init your app in this process.
-           return;
-       }
-       LeakCanary.install(this);
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                // This process is dedicated to LeakCanary for heap analysis.
+                // You should not init your app in this process.
+                return;
+            }
+            LeakCanary.install(this);
+        }
+
     }
 
     public AppCompoent getAppCompoent(){
